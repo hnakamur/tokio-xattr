fa18818923e03e4230ea681a9b05fd5c8ab50c12
Hiroaki Nakamura <hnakamur@gmail.com>
2019-08-07 22:50:10 +0900

    Rewrite with async_await feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
 #![deny(missing_docs, missing_debug_implementations)]
-#![doc(html_root_url = "https://docs.rs/tokio-xattr/0.1.0")]
+#![doc(html_root_url = "https://docs.rs/tokio-xattr/0.2.0")]
+#![feature(async_await)]
 
 //! A pure-Rust library to manage extended attributes asynchronously.
 //!
@@ -10,31 +11,35 @@
 //! This module uses tokio_threadpool to manage extended attributes
 //! asynchronously.
 
-extern crate futures;
-extern crate tokio_io;
-extern crate tokio_threadpool;
-
 mod xattr;
 
 pub use blocking_xattr::XAttrs;
 pub use xattr::{get, list, remove, set};
 
-use futures::Async::*;
-use futures::Poll;
-
 use std::io;
 use std::io::ErrorKind::Other;
+use std::task::Poll;
+use std::task::Poll::*;
 
-fn blocking_io<F, T>(f: F) -> Poll<T, io::Error>
+fn blocking_io<F, T>(f: F) -> Poll<io::Result<T>>
 where
     F: FnOnce() -> io::Result<T>,
 {
     match tokio_threadpool::blocking(f) {
-        Ok(Ready(Ok(v))) => Ok(v.into()),
-        Ok(Ready(Err(err))) => Err(err),
-        Ok(NotReady) => Ok(NotReady),
-        Err(_) => Err(blocking_err()),
+        Ready(Ok(v)) => Ready(v),
+        Ready(Err(_)) => Ready(Err(blocking_err())),
+        Pending => Pending,
     }
+}
+
+async fn asyncify<F, T>(f: F) -> io::Result<T>
+where
+    F: FnOnce() -> io::Result<T>,
+{
+    use futures_util::future::poll_fn;
+
+    let mut f = Some(f);
+    poll_fn(move |_| blocking_io(|| f.take().unwrap()())).await
 }
 
 fn blocking_err() -> io::Error {
